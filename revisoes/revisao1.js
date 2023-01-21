6211b553d1df3e0929f15440a144e7b597ea5d7e
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-01-21 10:28:01 -0300

    Update revisao1.js

--- revisoes/revisao1.js
+++ revisoes/revisao1.js
@@ -7,3 +7,34 @@ console.log(clone)
 const GrupoA = ['Joao', 'Maria', 'Daniel']
 const GrupoFinal = ['Larissa',...GrupoA, 'Rafaela']
 console.log(GrupoFinal)
+
+
+// arrow Function
+//Sintaxe reduzida
+const soma = (a , b) => a + b 
+console.log(soma(2, 3))
+
+const soma2 = (a , b) => {
+    return a +b 
+}  
+console.log(soma(2, 3))
+// sempre vai ser uma função Anonima.
+
+// Arrow Function (this)
+const lexico1 = () => console.log(this === exports)
+lexico1()
+
+//parametro default
+function log(texto = 'Node') {
+    console.log(texto)
+}
+log()
+log('Sou mais forte')
+
+// Operador Rest
+function total(...numeros) {
+    let total = 0
+    numeros.forEach(n => total += n )
+    return total
+}
+console.log(total(3, 2, 4,))
