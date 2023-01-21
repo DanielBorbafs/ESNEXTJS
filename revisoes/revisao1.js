7a246aa2e3c085335710c6502c5680fc6d24e369
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-01-21 10:30:30 -0300

    Update revisao1.js

--- revisoes/revisao1.js
+++ revisoes/revisao1.js
@@ -38,3 +38,27 @@ function total(...numeros) {
     return total
 }
 console.log(total(3, 2, 4,))
+
+// let e const
+//bloco
+{
+    var a = 2
+    let b = 3
+    console.log(b)
+}
+console.log(a)
+
+
+// template STRING
+const produto = 'Ipad'
+console.log(`${produto} é caro`)
+
+// Destructuring - desestruturando váriaveis, arrays e etc..
+const [l, e, ...tras] = "Cod3r"
+console.log(l, e, tras)
+
+const [x,y] = [1, 2, 3]
+console.log(x, y)
+
+const {idade, nome} = {nome: 'Ana', idade: 9}
+console.log(idade , nome)
